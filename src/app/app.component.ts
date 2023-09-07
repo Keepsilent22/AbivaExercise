2c07f1c19ac6dbf58ec2a84782d0ee8f37058b11
Keepsilent22 <racelis_rhenan2000@yahoo.com>
2023-09-07 13:00:32 +0800

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  @ViewChild('lastContainer') lastContainer!: ElementRef;
+  constructor() { }
+  scrollToLastContainer() {
+    const containerElement = this.lastContainer.nativeElement;
+    containerElement.scrollIntoView({ behavior: 'smooth' });
+  }
+
   title = 'Exercise';
+  formData = {
+    Fname: '',
+    Lname: '',
+    email: '',
+    role: ''
+  };
+  submitForm() {
+    console.log(this.formData);
+    this.formData = {
+      Fname: '',
+      Lname: '',
+      email: '',
+      role: ''
+    };
+  }
 }
